eb8d5949e67eef80b0195753e729c9c9fd3e6114
Dariya <dkldova@list.ru>
2022-08-06 19:49:32 +0300

    create task24

--- task24/Program.cs
+++ task24/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//7 -> 28
+//4 -> 10
+//8 -> 36
+
+Console.Write("Введите число А: ");
+int A  = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+
+for (int i = 1; i <= A; i++)
+{
+    summ = summ + i;
+}
+
+ Console.WriteLine($"Результат выполнения программы = {summ}");
