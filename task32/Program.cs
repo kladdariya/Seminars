229d03b2cde7b7a5189dcc4e05700d4a315b54d6
Dariya <dkladova@list.ru>
2022-08-12 16:46:29 +0300

    cteate task 34

--- task32/Program.cs
+++ task32/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] FillArrayWithRandomNumber(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int [size];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+int[] array = FillArrayWithRandomNumber(12, -9, 9);
+Console.WriteLine('[' + string.Join(",", array) + ']');
+int[] arr = array.Select(r=> r * (-1)).ToArray();
+Console.WriteLine('[' + string.Join(",", arr) + ']');
---
+++ task34/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//[345, 897, 568, 234] -> 2
+
+int []FillArrayWithRandomNumber(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int [size];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+int[] array = FillArrayWithRandomNumber(4,100,1000);
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] % 2 == 0)
+{
+    count ++;
+}
+}
+Console.WriteLine('[' + string.Join(",", array) + ']');
+Console.WriteLine($"Количество четных чисел = {count}");
+      
+
+
+
