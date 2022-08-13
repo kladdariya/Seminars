e60ba043b7a464e87e57b3bd8590919e458aafe5
Dariya <dkladova@list.ru>
2022-08-13 15:44:35 +0300

    add unfo in task

--- task38/Program.cs
+++ task38/Program.cs
@@ -1,11 +1,11 @@
-﻿// Задача 38: Задайте с клавиатуры массив вещественных (double) чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// Задача 38: Задайте с клавиатуры массив вещественных (double) чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // Задать размер (int) массива с клавиатуры
 // [3 7 22 2 78] -> 76
 // ввод массива с клавиатуры
 
 Console.WriteLine("Введите размер массива: ");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] a = new int[size];
+double size = Convert.ToInt32(Console.ReadLine());
+double[] a = new double[size];
 for(int i = 0; i<size ;i++)
 {
 Console.WriteLine("Введите {0}-й элемент", i + 1);
@@ -13,8 +13,8 @@ a[i] = Convert.ToInt32(Console.ReadLine());
 }
 
 Console.WriteLine('[' + string.Join(",", a) + ']');
-int max = a[0];
-int min = a[0];
+double max = a[0];
+double min = a[0];
 for ( int i = 0; i < size; i++)
 {
     if (a[i] > max)
