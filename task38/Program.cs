9d19830e0009c38b54050510a1edda9fdf280f9e
Dariya <dkladova@list.ru>
2022-08-18 23:15:46 +0300

    create task 41

--- task38/Program.cs
+++ task38/Program.cs
@@ -1,11 +1,11 @@
-// Задача 38: Задайте с клавиатуры массив вещественных (double) чисел. Найдите разницу между максимальным и минимальным элементов массива.
+﻿// Задача 38: Задайте с клавиатуры массив вещественных (double) чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // Задать размер (int) массива с клавиатуры
 // [3 7 22 2 78] -> 76
 // ввод массива с клавиатуры
 
 Console.WriteLine("Введите размер массива: ");
-double size = Convert.ToInt32(Console.ReadLine());
-double[] a = new double[size];
+int size = Convert.ToInt32(Console.ReadLine());
+int[] a = new int[size];
 for(int i = 0; i<size ;i++)
 {
 Console.WriteLine("Введите {0}-й элемент", i + 1);
@@ -13,8 +13,8 @@ a[i] = Convert.ToInt32(Console.ReadLine());
 }
 
 Console.WriteLine('[' + string.Join(",", a) + ']');
-double max = a[0];
-double min = a[0];
+int max = a[0];
+int min = a[0];
 for ( int i = 0; i < size; i++)
 {
     if (a[i] > max)
@@ -28,4 +28,4 @@ for ( int i = 0; i < size; i++)
 }
 Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
 
-//  надо исправить на double
+// не работает! надо исправить
---
+++ task41/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел в одну линию (не используем Enter). Числа разделены пробелами. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите число М: ");
+int[] numbers = Console.ReadLine().Split().Select(x => Convert.ToInt32(x)).ToArray();
+
+Console.WriteLine('[' + string.Join(",", numbers) + ']');
+ int count = 0;
+ for (int i = 0; i<numbers.Length; i++)
+{
+     if (numbers[i] > 0)
+     {
+        count= count+1;
+     }
+}
+
+Console.WriteLine($"Количество чисел больше 0 = {count}");
