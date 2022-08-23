ca9e6798c848fe0a9800dc3f000bd07e89bee4d6
Dariya <dkladova@list.ru>
2022-08-23 23:30:57 +0300

    create task 50

---
+++ task50/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Console.Write("Введите m = ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n = ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = new Random().Next(1, 10);
+        Console.Write(matrix[i, j] + " ");
+
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите первую координату позиции элемента  ");
+int cos = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первую координату позиции элемента  ");
+int pos = Convert.ToInt32(Console.ReadLine());
+if (cos<m-1 && pos<n-1)
+{
+    Console.WriteLine($"Значение элемента {matrix[cos,pos]}");
+}
+else
+{
+    Console.WriteLine($"Числа с координатами [{cos},{pos}] нет в массиве");
+}
+
+
+
+
+
