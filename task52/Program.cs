f64131888d0266687a8e88408489f35393e4728d
Dariya <dkladova@list.ru>
2022-08-25 12:28:37 +0300

    create task 52

---
+++ task52/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] FillRandomDoubleMatrix(int lines = 4, int colums = 4, int leftRange =1, int rightRange = 10)
+{
+    Random rand = new Random();
+    int[,] matr = new int[lines, colums];
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+       for (int j =0; j < matr.GetLength(1); j++);
+        {
+            matr[i,j] = rand.Next(leftRange, rightRange + 1);
+            Console.Write(matr[i, j] + " ");
+        } 
+    
+    }
+    return matr;
+}
+
+
+int[] matrix = FillRandomDoubleMatrix();
