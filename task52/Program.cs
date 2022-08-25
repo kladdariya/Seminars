8248e3f40c6b227543ad0614a16dda4a185442fc
Dariya <dkladova@list.ru>
2022-08-25 14:48:18 +0300

    add info in task52

--- task52/Program.cs
+++ task52/Program.cs
@@ -1,21 +1,47 @@
 ﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
-int[,] FillRandomDoubleMatrix(int lines = 4, int colums = 4, int leftRange =1, int rightRange = 10)
+int[,] FillRandomDoubleArray(int lines = 4, int colums = 4, int leftRange = 1, int rightRange = 10)
 {
+    int[,] randomArray = new int[lines, colums];
     Random rand = new Random();
-    int[,] matr = new int[lines, colums];
-
-    for (int i = 0; i < matr.GetLength(0); i++)
+    for (int i = 0; i < randomArray.GetLength(0); i++)
     {
-       for (int j =0; j < matr.GetLength(1); j++);
+        for (int j = 0; j < randomArray.GetLength(1); j++)
         {
-            matr[i,j] = rand.Next(leftRange, rightRange + 1);
-            Console.Write(matr[i, j] + " ");
-        } 
-    
+            randomArray[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+
     }
-    return matr;
+    return randomArray;
+
 }
 
+int[,] array = FillRandomDoubleArray();
+PrintMatrix(array);
 
-int[] matrix = FillRandomDoubleMatrix();
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    double result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = sum + array[i, j];
+
+
+    }
+    
+    result = sum / array.GetLength(1);
+    Console.Write($"{result}; ");
+}
