cdc89e1de8f351a63ac0772a5a0f062463ed48dc
Dariya <dkladova@list.ru>
2022-08-28 15:31:55 +0300

    add task4,5,10,13,15

---
+++ task13/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+
+if (number < 100)
+{
+   Console.WriteLine ("Третьей цифры нет"); 
+   
+   
+}
+else 
+{
+    while(number>999)
+    {
+        number = number/10;
+    }
+  
+Console.WriteLine(number%10);
+}
+
---
+++ task15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите цифру, обозначающую день недели: ");
+
+int namberDay = Convert.ToInt32(Console.ReadLine());
+
+if (namberDay == 6 || namberDay == 7)
+{
+    Console.WriteLine("Да");
+}
+else 
+
+if (namberDay >= 1 && namberDay <= 5)
+    {
+        Console.WriteLine("Нет");
+    }
+ else 
+ {
+    Console.WriteLine("Это не день недели");
+ }
