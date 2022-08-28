d72ad7321c67f45843fe1cab1d877cd0e72e8aae
Dariya <dkladova@list.ru>
2022-08-28 12:33:23 +0300

    add info readmy

--- task66/Program.cs
+++ task66/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 66: Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
