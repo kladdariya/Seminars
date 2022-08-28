69fbd781261eb68aeb8e2f2386157a769962abb7
Dariya <dkladova@list.ru>
2022-08-28 15:10:17 +0300

    add info task68

--- task66/Program.cs
+++ task66/Program.cs
@@ -2,12 +2,12 @@
 //Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
 Console.WriteLine("Введите число М ");
-int M = Convert.ToInt32(Console.ReadLine());
+int numM = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите число N ");
-int N = Convert.ToInt32(Console.ReadLine());
+int numN = Convert.ToInt32(Console.ReadLine());
 
-int res = SetSumDigits(N,M);
+int res = SetSumDigits(numN,numM);
 Console.WriteLine(res);
 
 int SetSumDigits(int N, int M)
---
+++ task68/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+
+Console.WriteLine("Введите положительное число M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите положительное число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+ int result = FunctionAkker(numM,numN);
+ Console.WriteLine(result);
+
+int FunctionAkker(int M, int N)
+{
+    if (M == 0) 
+    return N + 1;
+    else 
+    if (N == 0) 
+    return FunctionAkker(M - 1, 1);
+
+    return FunctionAkker(M - 1, FunctionAkker(M, N - 1));
+}
